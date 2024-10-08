8cbc56f853f7cf76e37ab431ff5f3caf5f06d733
Flodsc <florenciac0812@gmail.com>
2024-10-08 00:19:55 +0000

    Agregué la funcionalidad del semáforo
    - Implementé el estado para manejar el color del semáforo.
    - Añadí eventos `onClick` para cada luz del semáforo.
    - Actualicé la clase visual de las luces según el color seleccionado.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react"; 
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color,setColor] = useState(""); 
+
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
@@ -14,6 +16,23 @@ const Home = () => {
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
+			<div className="traffic-light-container">
+                {/* Componente del semáforo */}
+                <div className="traffic-light">
+                    <div
+                        className={`light red ${color === "red" ? "selected" : ""}`}
+                        onClick={() => setColor("red")}
+                    ></div>
+                    <div
+                        className={`light yellow ${color === "yellow" ? "selected" : ""}`}
+                        onClick={() => setColor("yellow")}
+                    ></div>
+                    <div
+                        className={`light green ${color === "green" ? "selected" : ""}`}
+                        onClick={() => setColor("green")}
+                    ></div>
+                </div>
+            </div>
 			<p>
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
